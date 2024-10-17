6bd7433eb4493f06a3653e22cb18ea8ff45bb8a8
MALAPAKA DATTA VENKATA NAGA SAI DEEPAK <154827923+DeepakMalapaka@users.noreply.github.com>
2024-10-17 11:10:58 +0530

    Add files via upload

---
+++ Multiple_catches.java
@@ -0,0 +1,17 @@
+public class Multiple_catches {
+    public static void main(String[] args) {
+        try {
+            int a=10/0;
+            System.out.println(a);
+        } catch (ArithmeticException e) {
+            System.out.println("Arithematic exception");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array out of bound exception");
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Null pointer exception");
+        }
+    }
+}
